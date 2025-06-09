58107ea4c790d94f1673d69dd0d15b1f388854b2
eklownr <eklownr@gmail.com>
2025-06-09 10:48:35 +0200

    new file menu.py and main rename to run.py

--- print_ask_date.py
+++ print_ask_date.py
@@ -1,7 +1,12 @@
 import json
+import sys
 from food_helper import print_date_file
 
-anser = input("(Hit Enter to open date.json) or type in Filename to open: ")
-if anser == "":
-    anser = "json/date.json"
-print_date_file(anser)
+arg = sys.argv
+if len(arg) > 1:
+    print_date_file(arg[1])
+else:
+    anser = input("(Hit Enter to open date.json) or type in the filename to open: ")
+    if anser == "":
+        anser = "json/date.json"
+    print_date_file(anser)
