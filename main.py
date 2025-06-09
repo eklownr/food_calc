8534d225ca27891c391c838d85ab791b716f56c9
eklownr <eklownr@gmail.com>
2025-06-09 17:38:17 +0200

    add menu.md to main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from rich import print
 from run import add_food
-from menu import TestApp
+from rich.console import Console
+from rich.markdown import Markdown
+
+console = Console()
+def menu():
+    with open("menu.md") as readme:
+        markdown = Markdown(readme.read())
+    console.print(markdown)
+
 
 run = True
 while run:
-    answer = input(" 'q' - quit\n 'm' - menu\n 'r' - run_foodApp: ")
+    answer = input(" m - menu: ")
     if answer == "q":
         print("Bye")
         run = False
     elif answer == "m":
-        # menu
-        app = TestApp()
-        app.run()
-
+        menu()
     elif answer == "r":
-        # Run food app
         add_food()
     else:
         print("wrong value: q - m - r")
---
+++ menu.md
@@ -0,0 +1,9 @@
+# Menu
+* q - quit 
+* m - menu
+* r - run food app
+* k - Kcal % FAT, Carbs, Protein
+* p - print all_food Data Base
+* n - add new food to all_food_db.json
+* a - 
+------
